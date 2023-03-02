b837f9e9a91e6144edfb317373d2951eaa828585
unknown <steffielee26@gmail.com>
2023-03-02 11:49:12 +0800

    smaller graphs

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,39 +1,46 @@
 <template>
     <v-layout>
         <v-container>
-            <v-row>
+            <v-row justify ="center">
+                    <v-col align="center">
+                    <v-col cols="8">
+                        
+                        <v-card class="mb-10 pa-3">
+                            <BarGraph/>
+                        </v-card>
+                    </v-col>
+                    
 
-                <v-col>
-
-                    <v-card class="mb-10 pa-3">
-                        <BarGraph />
-                    </v-card>
                     <v-row justify="center">
-                        <v-col cols="6" class="pt-12">
-                            <v-card class="pa-3">
+                        <v-col cols="4" class="pt-12">
+                            <v-card class="pa-3 elevation-3">
                                 <GenderPieGraph />
                             </v-card>
                         </v-col>
-                        <v-col cols="6" class="pt-12">
+                        <v-col cols="4" class="pt-12">
                             <v-card class="pa-3">
                                 <AgePieGraph />
                             </v-card>
                         </v-col>
                     </v-row>
+
                     <v-row justify="center">
-                        <v-col cols="6" class="pt-12">
+
+                        <v-col cols="4" class="pt-12">
                             <v-card class="pa-3">
                                 <ReasonForVisitPieGraph />
                             </v-card>
                         </v-col>
-                        <v-col cols="6" class="pt-12">
+
+                        <v-col cols="4" class="pt-12">
                             <v-card class="pa-3">
                                 <ActionProvidedPieGraph />
                             </v-card>
                         </v-col>
+                        
                     </v-row>
-
                 </v-col>
+                
             </v-row>
         </v-container>
     </v-layout>
