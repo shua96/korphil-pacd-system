ac529ce6f535ff9d8d8e1a22cbb82b7d97325c80
krstlkna <chrstllkna@gmail.com>
2023-05-09 19:57:34 +0800

    updated assessment client page/added CRUD using axios/working

--- src/views/admin-side/AssessmentData.vue
+++ src/views/admin-side/AssessmentData.vue
@@ -1,5 +1,5 @@
 <template>
-    <h1 class="pl-10 mb-10 ">Assessment Client Data</h1>
+    <h1 class="pl-10 mb-10 ">Client Data</h1>
     <v-sheet style="border-radius: 15px; background-color: white;" class="px-16 pt-5 mx-10 mb-5 elevation-1">
         <v-row no-gutters>
             <v-col>
@@ -28,48 +28,63 @@
             </v-col>
         </v-row>
     </v-sheet>
-    <v-data-table :search="search" :headers="headers" :sortByYear="sortByYear" :items="data"
+    <v-data-table :search="search" :sortByYear="sortByYear" :headers="headers" :items="data"
         class="v-table elevation-1 pt-5 mx-10 mb-5" style="border-radius: 15px;">
         <template v-slot:top>
             <v-toolbar flat>
                 <v-toolbar-title>Client Information</v-toolbar-title>
                 <v-spacer></v-spacer>
-                <v-dialog v-model="dialog" max-width="500px">
+                <v-dialog v-model="dialog" max-width="500px" persistent>
                     <template v-slot:activator="{ props }">
-                        <v-btn class="mb-2 elevation-1" style="background-color: #3C59A6; color: white;" v-bind="props">
+                        <v-btn class="mb-2 mr-2 elevation-1" style="background-color: #3C59A6; color: white;"
+                            v-bind="props">
                             New Item
+                        </v-btn>
+                        <v-btn class="mb-2 elevation-1" style="background-color: #3C59A6; color: white;"
+                            @click="printItems()">
+                            Refresh
                         </v-btn>
                     </template>
                     <v-sheet>
                         <v-card-title>
-                            <span class="text-h5">{{ formTitle }}</span>
+                            <span class="text-h5">{{ getFormTitle() }}</span>
                         </v-card-title>
 
                         <v-card-text>
                             <v-container>
                                 <v-row>
                                     <v-col cols="12" sm="6" md="4">
-                                        <v-text-field v-model="editedItem.name" label="Name"></v-text-field>
+                                        <v-text-field v-model="editedItem.firstname" label="First Name"></v-text-field>
                                     </v-col>
                                     <v-col cols="12" sm="6" md="4">
-                                        <v-text-field v-model="editedItem.age" label="Age" type="number"></v-text-field>
+                                        <v-text-field v-model="editedItem.middlename" label="Middle Name"></v-text-field>
                                     </v-col>
                                     <v-col cols="12" sm="6" md="4">
-                                        <v-select v-model="editedItem.gender" label="Gender" :items="['Male', 'Female']">
-                                        </v-select>
+                                        <v-text-field v-model="editedItem.lastname" label="Last Name"></v-text-field>
                                     </v-col>
                                     <v-col cols="12" sm="6" md="4">
-                                        <v-text-field v-model="editedItem.qualification"
-                                            label="Qualification"></v-text-field>
+                                        <v-text-field v-model="editedItem.age" type="number" label="Age"></v-text-field>
+                                    </v-col>
+                                    <v-col cols="12" sm="6" md="4">
+                                        <v-select :items="['Male', 'Female']" v-model="editedItem.sex"
+                                            label="Sex"></v-select>
+                                    </v-col>
+                                    <v-col cols="12" sm="6" md="4">
+                                        <v-text-field v-model="editedItem.contact" type="number"
+                                            label="Contact No."></v-text-field>
+                                    </v-col>
+                                    <v-col cols="12" sm="6" md="4">
+                                        <v-text-field v-model="editedItem.email" label="Email Address"></v-text-field>
                                     </v-col>
                                     <v-col cols="12" sm="6" md="4">
                                         <v-text-field v-model="editedItem.address" label="Address"></v-text-field>
                                     </v-col>
-                                    <v-col cols="12" sm="6" md="4">
-                                        <v-text-field v-model="editedItem.course" label="Course/Year"></v-text-field>
-                                    </v-col>
-                                    <v-col cols="12" sm="6" md="4">
-                                        <v-text-field v-model="editedItem.school" label="School/Institution"></v-text-field>
+
+                                </v-row>
+                                <v-row cols="12">
+                                    <v-col cols="12" sm="12" md="12">
+                                        <v-text-field v-model="editedItem.actionprovided" label="Action Provided"
+                                            class="text-caption"></v-text-field>
                                     </v-col>
                                 </v-row>
                             </v-container>
@@ -99,20 +114,68 @@
                             <v-btn class="text-capitalize px-5 border-button" style="border-color: #B4B2B2;" variant="flat"
                                 @click="closeDelete()">Cancel</v-btn>
                             <v-btn class="text-capitalize px-5 border-button" style="border-color: #E12727;" variant="flat"
-                                @click="deleteItemConfirm">Delete</v-btn>
+                                @click="deleteItemConfirm(editedItem)">Delete {{ date }}</v-btn>
                             <v-spacer></v-spacer>
 
                         </v-card-actions>
                     </v-sheet>
                 </v-dialog>
+
+                <v-dialog v-model="showMoreDialog" max-width="500px" color="error">
+                    <v-sheet class="pa-2">
+                        <v-icon size="x-large" color="#E12727"
+                            style="display: flex; margin: auto;">mdi-alert-circle-outline</v-icon>
+                        <v-card-title class="text-h5" style="display: flex; justify-content: center;">Are you
+                            sure?</v-card-title>
+                        <p class="text-h7" style="display: flex; justify-content: center;">Do you really want
+                            to delete this item?</p>
+                        <h1>{{ data }}</h1>
+                        <v-card-actions class="mt-6">
+                            <v-spacer></v-spacer>
+                            <v-btn class="text-capitalize px-5 border-button" style="border-color: #B4B2B2;" variant="flat"
+                                @click="showMoreDialog = false">Cancel</v-btn>
+
+                        </v-card-actions>
+                    </v-sheet>
+                </v-dialog>
             </v-toolbar>
+            <v-snackbar v-model="EditSnackbar" :timeout="timeout" color="info" vertical>
+                <v-icon size="large" class="mr-2">mdi-check-circle-outline</v-icon>
+                {{ getSnackbarText() }}
+
+                <template v-slot:actions>
+                    <v-btn color="white" variant="text" @click="EditSnackbar = false">
+                        Close
+                    </v-btn>
+                </template>
+            </v-snackbar>
         </template>
+
+
+        <template v-slot:item.fullname="{ item }">
+            {{ item.raw.firstname }}
+            {{ item.raw.lastname }}
+        </template>
+
+        <template v-slot:item._created_at="{ item }">
+            {{ moment(item.raw.created_at).format('YYYY-MM-DD') }}
+        </template>
+
+        <template v-slot:item._actionprovided="{ item }">
+            <div class="text-truncate">
+                {{ item.raw.actionprovided }}
+            </div>
+        </template>
+
         <template v-slot:item.actions="{ item }">
-            <v-icon size="small" class="me-2" @click="editItem(item.raw)">
+            <v-icon size="small" @click="editItem(item.raw)">
                 mdi-square-edit-outline
             </v-icon>
-            <v-icon size="small" @click="deleteItem(item.raw)" color="error">
+            <v-icon size="small" class="mx-2" @click="deleteItem(item.raw)" color="error">
                 mdi-trash-can-outline
+            </v-icon>
+            <v-icon size="small" @click="showMore(item.raw)">
+                mdi-plus
             </v-icon>
         </template>
         <template v-slot:no-data>
@@ -120,75 +183,104 @@
                 No Data Available
             </v-btn>
         </template>
+
     </v-data-table>
-    <v-main class="ml-14" color="primary"></v-main>
+    <v-main></v-main>
 </template>
 
 <script setup>
-import { ref, computed } from 'vue'
+import { ref, onMounted } from 'vue'
+import axios from 'axios'
+import moment from 'moment';
+import { computed } from '@vue/reactivity';
 
+onMounted(printItems);
+
+const headers = [
+    { title: 'Date', align: 'start', sortable: false, key: '_created_at' },
+    { title: 'Name', align: 'start', sortable: true, key: 'fullname' },
+    { title: 'Age', align: 'start', sortable: false, key: 'age' },
+    { title: 'Sex', align: 'start', sortable: false, key: 'sex' },
+    { title: 'Contact No.', align: 'start', sortable: false, key: 'contact' },
+    { title: 'Email Adr', align: 'start', sortable: false, key: 'email' },
+    { title: 'Address', align: 'start', sortable: false, key: 'address' },
+    { title: 'Action Provided', align: 'start', sortable: false, key: 'actionprovided', class: 'truncate' },
+    { title: 'Actions', align: 'start', sortable: false, key: 'actions' },
+]
+
+const EditSnackbar = ref(false)
+const showMoreDialog = ref(false)
+const data = ref([]);
+const search = ref('')
 const dialog = ref(false)
 const dialogDelete = ref(false)
-const search = ref('')
 const editedIndex = ref(-1)
-const sortByYear = ref('')
-let headers = [
-    {
-        title: 'Name',
-        align: 'start',
-        key: 'name',
-    },
-    { title: 'Age', key: 'age', sortable: false },
-    { title: 'Gender', key: 'gender', sortable: false },
-    { title: 'Qualification', key: 'qualification', sortable: false },
-    { title: 'Course/Year', key: 'course', sortable: false },
-    { title: 'School/Institution', key: 'school', sortable: false },
-    { title: 'Address', key: 'address', sortable: false },
-    { title: 'Actions', key: 'actions' },
-]
-let data = [
-    { name: 'John Doe' },
-    { name: 'Curtis Carpenter' },
-    { name: 'Presley Rodgers' },
-    { name: 'Diego Estrada' },
-    { name: 'Aedan Rivera' },
-]
-const editedItem = ref({
-    name: '',
-    age: '',
-    gender: '',
-    qualification: '',
-    address: '',
-    course: '',
-    school: '',
-})
+const sortByYear = ref('');
 const years = computed(() => {
     const year = new Date().getFullYear();
     return Array.from({ length: year - 1900 }, (_, index) => 1901 + index);
 });
+
+const editedItem = ref({
+    firstname: '',
+    middlename: '',
+    lastname: '',
+    age: '',
+    sex: '',
+    contact: '',
+    email: '',
+    address: '',
+})
+
+// function getSnackbarText() {
+//     console.log(editedIndex.value)
+//     return editedIndex.value === -1 ? 'Item saved successfully!' : 'Item edited successfully!';
+// }
+
+async function printItems() {
+    let response = await axios.get("http://localhost/pacd-system-api/public/api/get-assessmentclients");
+    data.value = response.data;
+}
+
+function getFormTitle() {
+    console.log(editedIndex.value)
+    return editedIndex.value === -1 ? 'New Item' : 'Edit Item';
+}
+
+function showMore() {
+    showMoreDialog.value = true
+}
+
 function editItem(item) {
     editedIndex.value = this.data.indexOf(item)
     editedItem.value = Object.assign({}, item)
     dialog.value = true
 }
-const formTitle = computed(() => {
-    return editedIndex.value === -1 ? 'Edit Item' : 'New Item'
-})
 
+// async function saveEditItem(item) {
+//     await axios.post("http://localhost/pacd-system-api/public/api/updateclient", editedItem.value);
+//     dialog.value = false
+//     printItems();
+// }
+
+async function save() {
+    if (editedIndex.value === -1) {
+        await axios.post("http://localhost/pacd-system-api/public/api/createassessmentclient", editedItem.value);
+        printItems();
+        this.close()
+        // EditSnackbar.value = true
+    } else if (editedIndex.value >= 0) {
+        await axios.post("http://localhost/pacd-system-api/public/api/update-assessmentclient", editedItem.value);
+        dialog.value = false
+        printItems();
+        // EditSnackbar.value = true
+    }
+}
 
 function close() {
     this.dialog = false
     this.editedItem = Object.assign({}, this.defaultItem)
     this.editedIndex = -1
-}
-
-function save() {
-    if (this.editedIndex > -1) {
-        Object.assign(this.data[this.editedIndex], this.editedItem)
-    } else {
-        this.data.push(this.editedItem)
-    }
-    this.close()
 }
 
 function deleteItem(item) {
@@ -197,9 +289,10 @@ function deleteItem(item) {
     this.dialogDelete = true
 }
 
-function deleteItemConfirm() {
-    this.data.splice(this.editedIndex, 1)
-    this.closeDelete()
+async function deleteItemConfirm(item) {
+    await axios.post('http://localhost/pacd-system-api/public/api/delete-assessmentclient', item);
+    dialogDelete.value = false;
+    printItems();
 }
 
 function closeDelete() {
@@ -207,6 +300,7 @@ function closeDelete() {
     this.editedItem = Object.assign({}, this.defaultItem)
     this.editedIndex = -1
 }
+
 </script>
 
 <style>
@@ -233,5 +327,12 @@ function closeDelete() {
 .v-icon--size-x-large {
     font-size: calc(var(--v-icon-size-multiplier) * 5em);
     margin: auto;
+}
+
+.text-truncate {
+    width: 250px;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
 }
 </style>
